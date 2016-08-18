00fb7f4a3ed4ad481df5914d8e5b0e87328409e9
Willard Songco <willardsongco@gmail.com>
2016-08-18 18:18:36 +0800

    Add files via upload

--- ProjectJWJ/src/ph/edu/dlsu/MainMenu.java
+++ ProjectJWJ/src/ph/edu/dlsu/MainMenu.java
@@ -6,16 +6,20 @@ import acm.util.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.Serializable;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 /**
  * Created by Willard on 8/12/2016.
  */
 
 
-public class MainMenu extends GraphicsProgram implements Serializable {
+public class MainMenu extends GraphicsProgram implements ActionListener {
 
 
     public static void main(String[] args) {
@@ -45,89 +49,69 @@ public class MainMenu extends GraphicsProgram implements Serializable {
 
     /**This is the title screen of the game */
     private void TitleScreen(){
-        GLabel titlescreen = new GLabel ("AFTERSTORY");
+        JLabel titlescreen = new JLabel ("AFTERSTORY", JLabel.CENTER);
         Font font = new Font("Matura MT Script Capitals", Font.BOLD, 48);
         titlescreen.setFont(font);
-        titlescreen.setColor(Color.WHITE);
+        titlescreen.setForeground(Color.darkGray);
         add(titlescreen, getWidth()/2 - 265, getHeight()/5);
     }
 
     /**For Button Label for new game */
     private void Newgamelabel() {
-        GRect box = new GRect(200, 50);
-        GLabel label = new GLabel("New Game", 200, 100);
-        label.setFont("Serif-bold-36");
-        box.setColor(Color.WHITE);
-        label.setColor(Color.WHITE);
-        add(box, getWidth()/2 - 90, getHeight()/3);
-        add(label, getWidth()/2 - 75, getHeight()/3 + 40);
+        JLabel label = new JLabel("New Game", JLabel.CENTER);
+        Font font = new Font ("Serif", Font.BOLD, 36);
+        label.setFont(font);
+        label.setForeground(Color.WHITE);
+        add(label, getWidth()/2 - 75, getHeight()/3);
     }
 
 
     /**For Button Label for load game */
     private void Loadgamelabel() {
-        GRect box = new GRect(200, 50);
-        GLabel label = new GLabel("Load Game", 200, 100);
-        label.setFont("Serif-bold-36");
-        box.setColor(Color.WHITE);
-        label.setColor(Color.WHITE);
-        add(box, getWidth()/2 - 90, getHeight()/3 + 70);
-        add(label, getWidth()/2 - 80, getHeight()/3 + 70 + 40);
+        JLabel label = new JLabel("Load Game", JLabel.CENTER);
+        Font font = new Font ("Serif", Font.BOLD, 36);
+        label.setFont(font);
+        label.setForeground(Color.WHITE);
+        add(label, getWidth()/2 - 80, getHeight()/3 + 70);
     }
 
     /**For Button label for options */
     private void Optionslabel() {
-        GRect box = new GRect(200, 50);
-        GLabel label = new GLabel("Options", 200, 100);
-        label.setFont("Serif-bold-36");
-        box.setColor(Color.WHITE);
-        label.setColor(Color.WHITE);
-        add(box, getWidth()/ 2 - 90, getHeight()/3 + 140);
-        add(label, getWidth()/ 2 - 50, getHeight()/3 + 140 + 40);
+        JLabel label = new JLabel("Options", JLabel.CENTER);
+        Font font = new Font ("Serif", Font.BOLD, 36);
+        label.setFont(font);
+        label.setForeground(Color.WHITE);
+        add(label, getWidth()/ 2 - 50, getHeight()/3 + 140);
+
+        //MILESTONE TWO: This is where my label functions as button. In this case, it's letting me open an options app
+        label.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                JFrame jf = new JFrame("Options");
+                jf.setBackground(Color.BLACK);
+                jf.setSize(new Dimension(600, 500));
+                jf.setVisible(true);
+
+            }
+        });
     }
 
     /**For Button label for quit */
     private void Quitlabel() {
-        GRect box = new GRect(200, 50);
-        GLabel label = new GLabel("Quit", 200, 100);
-        label.setFont("Serif-bold-36");
-        box.setColor(Color.WHITE);
-        label.setColor(Color.WHITE);
-        add(box, getWidth()/2 - 90, getHeight()/3 + 210);
-        add(label, getWidth()/2 - 30, getHeight()/3 + 210 + 40);
+        JLabel label = new JLabel("Quit", JLabel.CENTER);
+        Font font = new Font ("Serif", Font.BOLD, 36);
+        label.setFont(font);
+        label.setForeground(Color.WHITE);
+        add(label, getWidth()/2 - 30, getHeight()/3 + 210);
+
+        //MILESTONE TWO: This is where my label functions as button. In this case, it's letting me quit
+        label.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
     }
 
 //**MILESTONE TWO
-
-    /**To add the mouse*/
-    public synchronized void addMouseListener(MouseListener e) {
-        super.addMouseListener(e);
-    }
-
-    /**This is all the functions/actions when the buttons are pressed*/
-    public void mouseClicked(MouseEvent me) {
-        if (me.getX()>= getWidth()/2 - 90 && me.getX() <= getWidth()/2 - 90 + 200) {
-            /**Newgame*/
-            if (me.getY() >= getHeight()/3 && me.getY() <= getHeight()/3 - 50) {
-                //Please add function here
-                System.out.println("Hello");//Tester
-            }
-            /**Loadgame*/
-            else if (me.getY() >= getHeight()/3 + 70 && me.getY() <= getHeight()/3 + 70 + 50) {
-                //Please add function here
-                System.out.println("Hello");//Tester
-            }
-            /**Options*/
-            else if (me.getY() >= getHeight()/3 + 140 && me.getY() <= getHeight()/3 + 140 + 50) {
-                //Please add function here
-                System.out.println("Hello");//Tester
-            }
-            /**Quit*/
-            else if (me.getY() >= getHeight()/3 + 210 && me.getY() <= getHeight()/3 + 210 + 50) {
-                //Please add function here
-                System.out.println("Hello");//Tester
-            }
-        }
-    }
 
 }
